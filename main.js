d036c2982fbe4df0bb02303089c872522285a25f
Daniel <dhahaj@gmail.com>
2025-01-19 19:28:37 -0600

    Launch server process on app startup and ensure termination on quit; adjust main window size and enable DevTools

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const { spawn } = require("child_process");
 
 let mainWindow;
+let serverProcess;
 
 app.whenReady().then(() => {
+
+  serverProcess = spawn("node", [path.join(__dirname, "index.js")]);
+  serverProcess.stdout.on("data", (data) => {
+    console.log(`Server: ${data}`);
+  });
+
+  serverProcess.stderr.on("data", (data) => {
+    console.error(`Server Error: ${data}`);
+  });
+
+  serverProcess.on("close", (code) => {
+    console.log(`Server process exited with code ${code}`);
+  });
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1500,
+    height: 1100,
     webPreferences: {
       nodeIntegration: true, // Allow Node.js integration in the frontend
     },
@@ -16,7 +31,7 @@ app.whenReady().then(() => {
   mainWindow.loadFile(path.join(__dirname, "public/index.html"));
 
   // Open DevTools (optional for debugging)
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
   app.on("activate", () => {
     // Re-create the window if the app is re-opened on macOS
@@ -29,5 +44,11 @@ app.whenReady().then(() => {
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
+  }
+});
+
+app.on("quit", () => {
+  if (serverProcess) {
+    serverProcess.kill(); // Ensure the server process is terminated when the app quits
   }
 });
